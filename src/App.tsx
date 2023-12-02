a36b57d19b77ba74d7d9a8c02c49eea919e5d606
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-12-01 21:02:00 -0300

    projeto rocktseat

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './styles/global.scss'
+import { Header } from './components/Header'
+import { TaskList } from './components/TaskList'
+
+export default function App() {
+ 
+  return (
+    <>
+     <Header />
+     <TaskList />
+    </>
+  )
+}
+
